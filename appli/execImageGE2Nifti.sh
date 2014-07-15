e314fd7804eb74b9bad7b226e6a8bfa31d25c1f4
juanprietob <juanprietob@gmail.com>
2014-07-14 20:57:05 -0400

    Commit GE conversion
    GE conversion

---
+++ appli/execImageGE2Nifti.sh
@@ -0,0 +1,40 @@
+
+
+patientsDir=$1
+patientId=$2
+
+mkdir -p temp
+
+for patientDirs in $(ls -d ${patientsDir}/*${patientId}*);
+do
+	cp -r ${patientDirs} ./temp
+	patientDir=$(basename ${patientDirs})
+
+
+	for files in $(find ./temp/ -name *.gz);
+	do
+		gzip -d ${files}
+	done
+
+	sessionId=${patientDir##*_}
+
+	echo 'sessionId='${sessionId}
+	echo 'patientDir='${patientDir}
+
+
+	for imageTypes in $(ls -d ./temp/${patientDir}/*/)
+	do 
+		
+		for files in $(find ./temp/${patientDir}/${imageType} -name I.001)
+    	do 
+    		tempImageType=${files%%I.001}
+    		imageType=$(basename ${tempImageType})
+        	./ImageSeriesReadWrite -f $files -o ./temp/${patientId}_${sessionId}_${imageType}.nii.gz
+		done
+        
+	done
+
+	rm -rf ./temp/${patientDir}
+done
+
+
