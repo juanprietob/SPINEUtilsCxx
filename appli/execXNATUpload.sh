c359a6fda5a764a2ebc866d566e0f56537dc9ca9
juanprietob <juanprietob@gmail.com>
2014-07-25 13:32:05 -0400

    Commit menoBAtch2

--- appli/execXNATUpload.sh
+++ appli/execXNATUpload.sh
@@ -1,8 +1,9 @@
 
+imageDir=$1
 USER=jprieto
 PASSWORD=UncK-9Doggy
-PROJECT=NPMSC_MGHB
-SUBJECT=TEST_MGHB2
+PROJECT=T01
+SUBJECT=testSub
 SESSION=${SUBJECT}_MR1
 
 
@@ -13,7 +14,7 @@ do
     u=${outDir}/${dcm#${imageDir}*}
     erg=`curl -H 'Content-Type: application/dicom' \
         -X POST -u $USER:$PASSWORD \
-        "https://xnat.utahdcc.org/DCCxnat/data/services/import?inbody=true&PROJECT_ID=$PROJECT&SUBJECT_ID=$SUBJECT&EXPT_LABEL=$session" \
+        "https://xnat.utahdcc.org/DCCxnat/data/services/import?inbody=true&PROJECT_ID=$PROJECT&SUBJECT_ID=$SUBJECT&EXPT_LABEL=$SESSION" \
         --data-binary @$dcm | tr -d [:cntrl:]`
 
 
