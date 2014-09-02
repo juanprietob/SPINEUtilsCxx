70105d8441b2dbfd5012539285c3ed32ecc8b712
juanprietob <juanprietob@gmail.com>
2014-09-02 15:37:10 -0400

    Get contour info
    Get the contour info, area and perimeter in JSON format

---
+++ README.md
@@ -0,0 +1,19 @@
+SPINEUtilsCXX
+
+This project holds some of the back-end programs and utilities of SPINE
+
+The following can be found.
+
+SPINE contour manipulation.
+
+SPINEContours: Read; write; bezier interpolation; area and perimeter calculation.
+
+BoxPlots: link:view-source:https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=4&cad=rja&uact=8&ved=0CEAQFjAD&url=http%3A%2F%2Fcs.unc.edu%2F~yihong%2Findex_files%2Fweighted_functional_boxplot_miccai_2013.pdf&ei=MBgGVLyyHpHzgwSygYH4Aw&usg=AFQjCNE_yaxhXWFeWQBfqHF3mXf5j4Dosg&sig2=C7oOwWRkO35fLrFi0DVjYA&bvm=bv.74115972,d.eXY[Weighted Functional Boxplot with Application to Statistical Atlas Construction]
+    The boxPlots package produces statistical information from the contours.
+
+
+Image manipulation: Resample images to RAS space.
+
+
+Requirements:
+ITK, VTK, Xerces, CMake
---
+++ appli/SPINEContourInfo.cxx
@@ -0,0 +1,59 @@
+
+
+
+#include <iostream>
+
+
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkCollectionIterator.h>
+
+#include <SPINEContoursReader.h>
+#include <spinecontoursinterpolation.h>
+
+using namespace std;
+
+void help(string exec){
+    cerr<<"Usage: "<<exec<<" <contour filename>"<<endl;
+    cerr<<"Output: <contour filename>; areaContour1; areaContour2; ... ; areaContourN"<<endl;
+}
+
+int main(int argv, char** argc){
+
+    if(argv < 2){
+        help(string(argc[0]));
+
+        return 0;
+    }
+    vtkSmartPointer<SPINEContoursReader> sourcereader = vtkSmartPointer<SPINEContoursReader>::New();
+    sourcereader->SetFileName(argc[1]);
+    sourcereader->Update();
+
+    vtkPolyDataCollection* contours = sourcereader->GetOutput();
+    vtkCollectionSimpleIterator it;
+    contours->InitTraversal(it);
+
+    cout<<"{";
+    for(unsigned i = 0; i < contours->GetNumberOfItems(); i++){
+
+        vtkPolyData* nextpoly = contours->GetNextPolyData(it);
+
+        vtkSmartPointer<SPINEContoursInterpolation> contourinterpolation = vtkSmartPointer<SPINEContoursInterpolation>::New();
+        contourinterpolation->SetInputData(nextpoly);
+        contourinterpolation->Update();
+
+        cout<<"{\"id\": \""<<i<<"\", ";
+        cout<<"\"area\": \""<<contourinterpolation->GetArea()<<"\", ";
+        cout<<"\"perimeter\": \""<<contourinterpolation->GetContourLength()<<"\"";
+        cout<<"}";
+
+    }
+    cout<<"}";
+    cout<<endl;
+
+
+    return EXIT_SUCCESS;
+
+}
+
+
